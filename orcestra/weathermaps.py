23afc9bae0c5ad80c704154a4d1bc4ba5ca44522
Julia Windmiller <julia.windmiller@mpimet.mpg.de>
2024-07-29 18:15:26 +0200

    Changes layer to currently available one

--- orcestra/weathermaps.py
+++ orcestra/weathermaps.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def plot_sat(date_time_obj, 
-             layer = "VIIRS_SNPP_CorrectedReflectance_TrueColor,Coastlines_15m",
+             layer = "MODIS_Terra_CorrectedReflectance_TrueColor,Coastlines_15m",
              resolution = 0.05,  # in degree
              bbox = [-10.0, -65.0, 25.0, -5.0]  # S, W, N, E
             ):
