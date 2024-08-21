8a4599d2d48c61b1142b2fbb3054b4a1aee08771
Lukas Kluft <lukas.kluft@gmail.com>
2024-08-21 07:50:56 -0100

    Disable lat/lon deduplication by default

--- orcestra/meteor.py
+++ orcestra/meteor.py
@@ -48,8 +48,13 @@ def request_telemetry(user, password, mission_id="PERCUSION", vehicle_id="METEOR
     return r
 
 
-def get_meteor_track():
-    """Request the METEOR track from PLANET and return as xr.Dataset."""
+def get_meteor_track(deduplicate_latlon=False):
+    """Request the METEOR track from PLANET and return as xr.Dataset.
+
+    Args:
+      deduplicate_latlon (bool): Remove duplicated lat/lon pairs
+        for, e.g., faster plotting (default is `False`).
+    """
     # Retrieve telemetry data for METEOR
     user, password = get_planet_credentials()
     r = request_telemetry(user, password)
@@ -76,8 +81,10 @@ def get_meteor_track():
     # Properly parse datetimes
     ds = ds.assign_coords(time=[datetime.fromisoformat(str(t)) for t in ds.time.values])
 
-    # Remove duplicate lat/lon information
-    pos = [(lon, lat) for lon, lat in zip(ds.lon.values, ds.lat.values)]
-    idx = np.sum(np.diff(pos, axis=0, prepend=1000), axis=1).astype(bool)
+    if deduplicate_latlon:
+        # Remove duplicate lat/lon information
+        pos = [(lon, lat) for lon, lat in zip(ds.lon.values, ds.lat.values)]
+        idx = np.sum(np.diff(pos, axis=0, prepend=1000), axis=1).astype(bool)
+        ds = ds.isel(time=idx).sortby("time")
 
-    return ds.isel(time=idx).sortby("time")
+    return ds
